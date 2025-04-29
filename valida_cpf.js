dd3520bcd4d08bd83e5a29bfa33d697c355bc99e
taitwelve <taynascimento15@gmail.com>
2025-04-29 17:25:52 -0300

    fix: complete cpf validation function

--- valida_cpf.js
+++ valida_cpf.js
@@ -5,3 +5,13 @@ function removePontuacao(campo) {
 function todosDigitosIguais(cpf) {
     return /^(\d)\1{10}$/.test(cpf);
 }
+
+// Função principal para validar o CPF
+export default function ehUmCPF(campo) {
+    const cpfSemPontuacao = removePontuacao(campo);
+    
+    if (todosDigitosIguais(cpfSemPontuacao) || calculaDigitoVerificador(cpfSemPontuacao, 10) || calculaDigitoVerificador(cpfSemPontuacao, 11) ||  cpfSemPontuacao.match(/[A-Za-zÀ-ÿ]+/)) {        console.log("Dígitos iguais:",todosDigitosIguais(cpfSemPontuacao));
+        console.log("CPF sem pontuação:", cpfSemPontuacao);
+        campo.setCustomValidity('Esse CPF não é válido');
+    }
+}
